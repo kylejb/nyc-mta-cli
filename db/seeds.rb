550d918f0e910de95502b12e9ca4a5340242902b
kylejb <47502769+kylejb@users.noreply.github.com>
2020-12-14 13:13:27 -0500

    Commented out sample data, as depicted in demo.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,6 @@ table.each do |row|
     TrainStation.create(name: row["Stop Name"], stop_id: row["GTFS Stop ID"], route_id: row["Daytime Routes"], latitude: row["GTFS Latitude"], longitude: row["GTFS Longitude"])
 end
 
-user1 = User.create(name: "kylejb", password: "secure_password1")
-fav1 = FavoriteStation.create(user: user1, train_station: TrainStation.find_by(name: "Times Sq - 42 St"))
+# user1 = User.create(name: "kylejb", password: "secure_password1")
+# fav1 = FavoriteStation.create(user: user1, train_station: TrainStation.find_by(name: "Times Sq - 42 St"))
 
