d654677e164276153520f9f68cd51a6567735f82
kylejb <47502769+kylejb@users.noreply.github.com>
2021-02-16 13:00:04 -0500

    Added comments to supplement README

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,15 @@
+require 'csv'
+
 FavoriteStation.destroy_all
 User.destroy_all
 TrainStation.destoy_all
 
-require 'csv'
-
+# Populates Database with MTA Subway Station Stop information sourced from the MTA
 table = CSV.read(("./db/Stations.csv"), headers: true)
-
-table.each do |row| 
-    TrainStation.create(name: row["Stop Name"], stop_id: row["GTFS Stop ID"], route_id: row["Daytime Routes"], latitude: row["GTFS Latitude"], longitude: row["GTFS Longitude"])
+table.each do |row|
+  TrainStation.create(name: row["Stop Name"], stop_id: row["GTFS Stop ID"], route_id: row["Daytime Routes"], latitude: row["GTFS Latitude"], longitude: row["GTFS Longitude"])
 end
 
-# user1 = User.create(name: "kylejb", password: "secure_password1")
-# fav1 = FavoriteStation.create(user: user1, train_station: TrainStation.find_by(name: "Times Sq - 42 St"))
-
+## Remove '#' on Line 16 to initialize database with that data
+## Change the quoted values to a username and password of your choice
+# User.create(name: "user", password: "change_this_password")
