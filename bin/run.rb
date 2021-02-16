bea9a7d298ff885bb48202fa73adfd9789e2c015
kylejb <47502769+kylejb@users.noreply.github.com>
2021-02-16 12:29:37 -0500

    Refactored for readability

--- bin/run.rb
+++ bin/run.rb
@@ -1,6 +1,5 @@
 require_relative '../config/environment'
 require 'pry'
 
-cli = CommandLineInterface.new
-cli.greet
-cli.basic_login_and_run
+CommandLineInterface.greet
+CommandLineInterface.basic_login_and_run
