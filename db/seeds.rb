6ba1c0acf42702badeed1c62928731c89619e256
Kyle J. Burda <47502769+kylejb@users.noreply.github.com>
2026-01-27 12:29:39 -0500

    chore(db): update subway station list

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ User.destroy_all
 TrainStation.destroy_all
 
 # Seed database with MTA Subway Station information
-table = CSV.read(("./db/Stations.csv"), headers: true)
+table = CSV.read(("./db/MTA_Subway_Stations.csv"), headers: true)
 table.each do |row|
   TrainStation.create(name: row["Stop Name"], stop_id: row["GTFS Stop ID"], route_id: row["Daytime Routes"], latitude: row["GTFS Latitude"], longitude: row["GTFS Longitude"])
 end
